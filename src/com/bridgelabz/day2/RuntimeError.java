151f6be1725215e6b170b98beebd9541312415ed
23Shivani <shivani.gharve.2310@gmail.com>
2021-09-24 08:39:18 +0530

    Added: Run Time Error Program

---
+++ src/com/bridgelabz/day2/RuntimeError.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day2;
+
+public class RuntimeError {
+	public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        int c=(a-b)/0;
+        System.out.println("The value of b : " +c);
+    }
+
+}
