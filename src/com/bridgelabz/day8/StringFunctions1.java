e1f95f183e75f3c57222e86b5d897b81a9100333
23Shivani <shivani.gharve.2310@gmail.com>
2021-10-07 22:23:43 +0530

    Added: Permutation using iterative method Program

---
+++ src/com/bridgelabz/day8/StringFunctions1.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.day8;
+
+public class StringFunctions1 {
+	
+	  private static void permutations(String candidate, String remaining){
+	      if (remaining == null) {
+	    	  return;
+	      }
+	      if (remaining.length() == 0) {
+	    	  System.out.println(candidate);
+	      }
+	      	for (int i = 0; i < remaining.length(); i++){
+	      		String newCandidate = candidate + remaining.charAt(i);
+	            String newRemaining = remaining.substring(0, i) + remaining.substring(i + 1);
+	            permutations(newCandidate, newRemaining);
+	        }
+	  }
+	  public static void main(String[] args) {
+		  String str = "ABC";
+		  permutations("", str);
+	  }
+}
